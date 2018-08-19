c5346bb0e3f2ca919918ee820e33a07c120a4448
YanagiEiichi <admin@web-tinker.com>
2018-08-19 15:27:46 +0800

    1.3.1

--- umd.js
+++ umd.js
@@ -1,3 +1,22 @@
+/**/ void function(factory) {
+/**/   switch (true) {
+/**/   case typeof module === 'object' && module && 'exports' in module: // CommonJS
+/**/     module.exports = factory();
+/**/     if (module.exports === void 0) module.exports = {};
+/**/     break;
+/**/   case typeof define === 'function' && !!define.amd: // AMD
+/**/     define(factory);
+/**/     break;
+/**/   default: // Global
+/**/     var global = new Function('return this')();
+/**/     var name = 'TimePicker';
+/**/     /**/ try { /* Fuck IE8- */
+/**/     /**/   if (typeof execScript === 'object') execScript('var ' + name);
+/**/     /**/ } catch(error) {}
+/**/     global[name] = factory();
+/**/   }
+/**/ }(function() {
+
 var TimePicker;
 
 {
@@ -324,3 +343,9 @@ var TimePicker;
   }
 
 }
+
+
+return TimePicker;
+
+/**/ });
+
